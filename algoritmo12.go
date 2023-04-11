a358a082240b6a20155cefcecaa3a64fa8c73c9c
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-11 08:21:06 -0300

    Update algoritmo12.go

--- algoritmo12.go
+++ algoritmo12.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var x string
+	fmt.Println("Digite uma string:")
+	fmt.Scan(&x)
+
+	letras := strings.Split(x, "")
+
+	if len(letras)%2 != 0 {
+		palindromo := true
+		for i := 0; i < len(letras)/2; i++ {
+			if letras[i] != letras[len(letras)-1-i] {
+				palindromo = false
+				break
+			}
+		}
+		if palindromo {
+			fmt.Printf("É palindromo")
+		}
+
+	} else {
+		fmt.Printf("A string %x nao é palindromo", x)
+	}
+}
+
