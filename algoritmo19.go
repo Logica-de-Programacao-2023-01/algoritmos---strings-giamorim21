9f2e6aa2a1f0945e4cf5b94f5449182bd5a9aaf9
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-13 16:31:38 -0300

    Update algoritmo19.go

--- algoritmo19.go
+++ algoritmo19.go
@@ -1,1 +1,19 @@
 package main
+
+import "fmt"
+
+//Solicite ao usuário uma string
+//e imprima a mesma string invertida.
+
+func main() {
+	var s string
+	fmt.Println("Digite uma string:")
+	fmt.Scan(&s)
+
+	inverso := ""
+	for i := len(s) - 1; i >= 0; i-- {
+		inverso += string(s[i])
+	}
+	fmt.Println("String invertida:", inverso)
+}
+
