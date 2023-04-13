aef74dc906d4c68d35c81076be7e205a75e29f7d
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-13 16:01:26 -0300

    Update algoritmo15.go

--- algoritmo15.go
+++ algoritmo15.go
@@ -1,1 +1,32 @@
 package main
+
+//Solicite ao usuário uma string 
+//e substitua todas as vogais por '*' (asterisco).
+
+import "fmt"
+
+func main() {
+	var s string
+	fmt.Print("Digite uma palavra ou frase: ")
+	fmt.Scan(&s)
+
+	vogais := "aeiouAEIOU"
+	result := ""
+	for _, char := range s {
+		isvogal := false
+		for _, vogal := range vogais {
+			if char == vogal {
+				isvogal = true
+				break
+			}
+		}
+		if isvogal {
+			result += "*"
+		} else {
+			result += string(char)
+		}
+	}
+
+	fmt.Println("Resultado:", result)
+}
+
