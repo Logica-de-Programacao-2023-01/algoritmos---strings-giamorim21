68bd7375bc7206d02655fbc751af1c1044847d19
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-13 16:28:06 -0300

    Update algoritmo18.go

--- algoritmo18.go
+++ algoritmo18.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+//Solicite ao usuário uma string
+//e informe se ela contém somente números (0 a 9).
+
+func main() {
+	var s string
+	fmt.Println("Digite uma string:")
+	fmt.Scan(&s)
+
+	_, err := strconv.Atoi(s)
+	if err == nil {
+		fmt.Println("A string contém somente números")
+	} else {
+		fmt.Println("A string não contém somente números")
+	}
+}
+
