bf11a5a8121aa367d6dc901455348a432548c3d1
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-04-13 16:18:29 -0300

    Update algoritmo17.go

--- algoritmo17.go
+++ algoritmo17.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+//Solicite ao usuário uma string
+//e imprima somente as suas letras únicas
+//(que aparecem apenas uma vez).
+
+func main() {
+	var s string
+	fmt.Print("Digite uma string: ")
+	fmt.Scanln(&s)
+
+	freqMap := make(map[rune]int)
+	for _, char := range s {
+		freqMap[char]++
+	}
+
+	var letras []string
+	for _, char := range s {
+		if freqMap[char] == 1 {
+			letras = append(letras, string(char))
+		}
+	}
+	fmt.Println("Letras únicas:", strings.Join(letras, ""))
+}
+
